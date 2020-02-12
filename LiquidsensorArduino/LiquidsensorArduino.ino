da9db34bafd90999487fa2b00ac81486f8a5b0c3
Manos Gerothanasis <33942553+ManosGero@users.noreply.github.com>
2020-02-12 12:22:44 +0200

    Update LiquidsensorArduino.ino

--- LiquidsensorArduino/LiquidsensorArduino.ino
+++ LiquidsensorArduino/LiquidsensorArduino.ino
@@ -1,8 +1,11 @@
 /*
- * 
+ * Liquidsensor Arduino 
+ * This program running on Arduino Uno.
+ * it observes the level of the Liquid and updates the LCD screen about the level.
+ * when the liquid level is very high the SIM900 module sends a message to inform the user about the danger. 
  * Add the Lcd display and Sim900 module.
  * Lcd display used for showing messages to the user.
- * The Sim900 is a module that helps to send SMS messages.  
+ * The SIM900 is a module that helps to send SMS messages.  
  */
 
 // include the librarys code:
@@ -15,25 +18,24 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 boolean layer_Low = 0;
 boolean layer_Normal = 0; 
 boolean layer_High = 0;
-String layerText="";
+String layerText=""; 
+int counterTime=0;
 
-void sendSMS();
-
-
+void sendSMS(); // this faction send the SMS by the SMS900 module
 
 
 void setup() {
-
+  counterTime=0;
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print(" Liquid Sensor");
+  lcd.print(" Liquid Sensor  ");
   
   // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
   // Make sure that corresponds to the baud rate of your module
   SIM900.begin(19200);
   // Give time to your GSM shield log on to network
-  //delay(20000);   
+  delay(20000);   
   
   // Send the SMS
 
@@ -49,21 +51,21 @@ void loop() {
     if (layer_Low== 0 && layer_Normal == 0 && layer_High == 0){
       
         lcd.setCursor(0, 0);
-        lcd.print(" Liquid Sensor");      
+        lcd.print(" Liquid Sensor  ");      
         lcd.setCursor(0, 1);
         lcd.print("Liquid layer Low");
       }
     if (layer_Low== 1 && layer_Normal == 0 && layer_High == 0){ 
       
         lcd.setCursor(0, 0);
-        lcd.print(" Liquid Sensor");
+        lcd.print(" Liquid Sensor  ");
         lcd.setCursor(0, 1);
         lcd.print("Liquid layer Normal");
       }
     if (layer_Low== 1 && layer_Normal == 1 && layer_High == 0){
       
         lcd.setCursor(0, 0);
-        lcd.print(" Liquid Sensor");
+        lcd.print(" Liquid Sensor  ");
         lcd.setCursor(0, 1);
         lcd.print("Liquid layer High");
       }
@@ -74,6 +76,15 @@ void loop() {
         lcd.print("     Danger!    ");        
         lcd.setCursor(0, 1);
         lcd.print("Very High layer ");
+        if (counterTime == 0){
+          sendSMS();
+           lcd.setCursor(0, 0);
+        lcd.print(" Senting message  ");
+        delay(10000);
+          counterTime = 360000;
+          } else { 
+            counterTime = counterTime - 1 ;
+            }
       }
 
 }
@@ -92,7 +103,7 @@ void sendSMS() {
   delay(100);
   
   // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
-  SIM900.println("Message example from Arduino Uno."); 
+  SIM900.println("ATTENTION \nLiquid level VERY HIGH!!  \nMessage sent from LiquidSensorArduino. "); 
   delay(100);
 
   // End AT command with a ^Z, ASCII code 26
